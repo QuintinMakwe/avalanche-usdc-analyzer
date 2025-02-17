13b613d210d69c4afc26f48e97ff0cbdbdf067bf
Quintin <quintinmakwe@gmail.com>
2025-02-17 04:25:19 +0100

    add sys files

---
+++ src/config/env.schema.ts
@@ -0,0 +1,13 @@
+import * as Joi from 'joi';
+
+export const configValidationSchema = Joi.object({
+  // Blockchain RPC Configuration
+  AVALANCHE_RPC_URL: Joi.string().required(),
+  AVALANCHE_NETWORK: Joi.string().valid('mainnet', 'testnet').default('mainnet'),
+
+  REDIS_PASSWORD: Joi.string().required(),
+
+  POSTGRES_USER: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
+  POSTGRES_DATABASE: Joi.string().required(),
+});
