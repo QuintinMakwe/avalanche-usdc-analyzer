09623f65d32484407fc4eb9e7e0c39832bc7ee1f
Quintin <quintinmakwe@gmail.com>
2025-02-18 02:16:05 +0000

    wip: add api health controller

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,10 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { HealthCheck } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
-  constructor(private health: HealthCheckService) {}
-
   @Get()
   @HealthCheck()
   check() {
